30ae755d7796e535e9e59e798bbfd7b78155f39d
mahmoud <memodnet@gmail.com>
2017-07-29 15:33:42 +0200

    commit from home

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,24 +6,77 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class MyStaticCalss {
-        public static string Name;
-        private int BornYear;
-        static MyStaticCalss()//must parameterless
-        {
-            Name = "default name";
-            BornYear = 12;
-        }
-        public static void GetName() {
-            Console.WriteLine(Name);
-        }
-    }
+
 
     class Program
     {
+        public class Personinfo
+        {
+            public int Weight;
+            public int Floor;
+        }
         static void Main(string[] args)
         {
-            MyStaticCalss.GetName();
+            int[] A = { 40, 100, 80, 20 };
+            int[] B = { 3, 2, 2, 3 };
+            int Result = solution(A, B, 5, 2, 200);
+            Console.WriteLine(Result);
+        }
+        public static int solution(int[] A, int[] B, int M, int X, int Y)
+        {
+            int StopCount = 0;
+            int Queue = A.Length;
+            Dictionary<int, Personinfo> Peoplelist = new Dictionary<int, Personinfo>();
+            while (Queue > 0)
+            {
+                Peoplelist = GetPeoplelist(A, B, X, Y, A.Length - Queue);
+                StopCount += RunElevator(Peoplelist, ref Queue, M);
+
+            }
+            return StopCount;
+        }
+        public static Dictionary<int, Personinfo> GetPeoplelist(int[] A, int[] B, int X, int Y, int Startindex)
+        {
+            Dictionary<int, Personinfo> Peoplelist = new Dictionary<int, Personinfo>();
+            for (int i = Startindex; i <= A.Length - 1; i++)
+            {
+
+                if (Peoplelist.Sum(p => p.Value.Weight) < Y)
+                {
+                    Peoplelist.Add(i, new Personinfo { Weight = A[i], Floor = B[i] });
+                }
+                else
+                {
+                    Peoplelist.Remove(i - 1);
+                }
+
+                if (Peoplelist.Count > X)
+                {
+
+                    Peoplelist.Remove(i);
+                }
+            }
+            return Peoplelist;
+        }
+
+        public static int RunElevator(Dictionary<int, Personinfo> Peoplelist, ref int Queue, int M)
+        {
+            int StopCount = 0;
+            string FloorsDone = "";
+            Queue -= Peoplelist.Count;
+            foreach (var Person in Peoplelist)
+            {
+                if (!FloorsDone.Contains("[" + Person.Value.Floor.ToString() + "]"))
+                {
+                    if (Person.Value.Floor <= M)
+                    {
+                        ++StopCount;
+                        FloorsDone += "[" + Person.Value.Floor + "]";
+                    }
+                }
+            }
+            ++StopCount; // last stop
+            return StopCount;
         }
     }
 }
