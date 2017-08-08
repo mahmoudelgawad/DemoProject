3dd34abf0474d94d87abf36866a82e30fb73401f
mahmoudahmed <mag@crossworkers>
2017-08-08 09:43:23 +0200

    commit from work

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,14 +7,40 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
 
-
     class Program
     {
 
         static void Main(string[] args)
         {
+            Console.WriteLine(solution(new int[] { 1, 2, 1, 2, 3, 2 }));
 
-           
+        }
+
+        public static int solution(int[] A)
+        {
+            bool found = false;
+            int value = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int ii = 0; ii < A.Length; ii++)
+                {
+                    if (A[ii] == A[i])
+                    {
+                        if (ii != i)
+                        {
+                            found = true;
+                        }
+                    }
+                }
+                
+                if (!found)
+                {
+                    value = A[i];
+                }
+                found = false;
+
+            }
+            return value;
         }
 
 
