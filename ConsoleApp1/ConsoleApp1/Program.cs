e9106ed620a5ad46f19365dd0d0df508f130a0cd
mahmoud <memodnet@gmail.com>
2017-08-12 14:34:34 +0200

    commit from home

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,43 +13,10 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Console.WriteLine(solution("0 - 22 1985--324"));
+            
         }
 
-        public static string solution(string S)
-        {
-            string Result = "";
-
-            string Input = S.Replace("-", "").Replace(" ", "");
-            if (Input.ToList().Count < 2 || Input.ToList().Count > 100)
-            {
-                return "";
-            }
-            int i = 0;
-            foreach (char ch in Input)
-            {
-                Result += ch;
-                ++i;
-                if (i == 3)
-                {
-                    Result += '-';
-                    i = 0;
-                }
-            }
-            if (Result[Result.Length - 1] == '-') {
-                Result= Result.Remove(Result.Length - 1);
-            }
-            if (i == 1) {
-                char[] A = Result.ToArray();
-                int lastDashIndex = Result.LastIndexOf("-");
-                char temp = Result[lastDashIndex - 1];
-                A[lastDashIndex] = temp;
-                A[lastDashIndex-1] = '-';
-                Result = new string(A);
-            }
-
-            return Result;
-        }
+      
 
     }
 }
