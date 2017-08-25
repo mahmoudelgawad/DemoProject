b4808408c3e938c302eba74a978509ddc5de0385
mahmoud <memodnet@gmail.com>
2017-08-25 17:10:59 +0200

    commit from home

--- ConsoleApp1/ConsoleApp1/PermMissingElem.cs
+++ ConsoleApp1/ConsoleApp1/PermMissingElem.cs
@@ -7,16 +7,48 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+
     class PermMissingElem
     {
-       
+     
+
         public static int solution(int[] A)
         {
             int Total = A.Sum();
             int IdealTotal = Enumerable.Range(1, A.Length + 1).Sum();
             return IdealTotal - Total;
+
+        }
+        public static int TapeEquilibrium(int[] A)
+        {
+            List<int> Results = new List<int>();
+            for (int p = 1; p <= A.Length - 1; p++)
+            {
+                int first = 0, second = 0;
+                for (int i = 0; i < A.Length; i++)
+                {
+
+                    if (i < p)
+                    {
+                        first += A[i];
+                    }
+                    else
+                    {
+                        second += A[i];
+                    }
+                }
+                int diff = first - second;
+                Results.Add((diff < 0) ? diff * -1 : diff);
+            }
+            if (Results.Count == 0)
+            {
+                return 0;
+            }
+            return Results.Min();
+
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
         }
     }
 
- 
+
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,38 +13,10 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Console.WriteLine(solution(new int[] { 3,1,2,4,3}));
+   
         }
 
-        public static int solution(int[] A)
-        {
-            List<int> Results = new List<int>();
-            for (int p = 1; p <= A.Length - 1; p++)
-            {
-                int first = 0, second = 0;
-                for (int i = 0; i < A.Length; i++)
-                {
-
-                    if (i < p)
-                    {
-                        first += A[i];
-                    }
-                    else
-                    {
-                        second += A[i];
-                    }
-                }
-                int diff = first - second;
-                Results.Add((diff < 0) ? diff * -1 : diff);
-            }
-            if (Results.Count == 0)
-            {
-                return 0;
-            }
-            return Results.Min();
-
-            // write your code in C# 6.0 with .NET 4.5 (Mono)
-        }
+       
 
 
 
