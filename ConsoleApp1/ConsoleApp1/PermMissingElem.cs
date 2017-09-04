46adab74510ac5834badabe3da659f64b1936ab9
mahmoud <memodnet@gmail.com>
2017-09-04 14:09:27 +0200

    commit from home

--- ConsoleApp1/ConsoleApp1/PermMissingElem.cs
+++ ConsoleApp1/ConsoleApp1/PermMissingElem.cs
@@ -48,6 +48,68 @@ namespace ConsoleApp1
 
             // write your code in C# 6.0 with .NET 4.5 (Mono)
         }
+
+        // get winter length
+        public static int GetMinWinterLength(int[] T)
+        {
+            int WinterLength = 0;
+            List<int> WinterList = new List<int>();
+            foreach (int Deg in T)
+            {
+                if (Deg < 6) //winter
+                {
+                    ++WinterLength;
+                }
+                else // summer
+                {
+                    if (WinterLength > 0)
+                    {
+                        WinterList.Add(WinterLength);
+                        WinterLength = 0;
+                    }
+
+                }
+            }
+            if (WinterLength > 0)
+            {
+                WinterList.Add(WinterLength);
+            }
+
+            if (WinterList.Count > 0)
+            {
+                return WinterList.Min();
+            }
+            return 0;
+        }
+
+        public static String ConvertNamesToEmails(String S, String C)
+        {
+            List<string> NamesBefore = new List<string>();
+            List<string> Emails = new List<string>();
+            NamesBefore = S.Split(new string[] { "; " }, StringSplitOptions.None).ToList();
+            foreach (string Name in NamesBefore)
+            {
+                string CurrentName = Name;
+                CurrentName = CurrentName.Replace("-", "");
+                string[] AName = CurrentName.Split(' ');
+                if (AName.Length > 0)
+                {
+                    CurrentName = AName[0] + "_" + AName[AName.Length - 1];
+                }
+                else
+                {
+                    CurrentName = CurrentName.Replace(" ", "_");
+                }
+                int CountNo = Emails.FindAll(e => e == CurrentName.ToLower() + "@" + C.ToLower() + ".com").Count;
+                CurrentName = CurrentName.ToLower() + ((CountNo > 0) ? (CountNo + 1).ToString() : "") + "@" + C.ToLower() + ".com";
+                Emails.Add(CurrentName);
+            }
+            if (Emails.Count > 0)
+            {
+                return String.Join("; ", Emails);
+            }
+            return "";
+        }
     }
 
 
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,10 +13,13 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-   
+        
         }
 
-       
+      
+
+
+
 
 
 
