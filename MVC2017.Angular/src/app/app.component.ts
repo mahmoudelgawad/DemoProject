b7fc3425ab9b9bba9215a15f2004b0ae9258a5bb
mahmoud <memodnet@gmail.com>
2017-09-10 23:50:16 +0200

    commit from home

---
+++ MVC2017.Angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    //template:`mahmoud ahmed {{title}}`
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Mahmoud app';
+    name = '';
+}
