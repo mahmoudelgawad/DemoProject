8ad458c9913059489fafb03302ab50723d688760
mahmoud <memodnet@gmail.com>
2017-10-26 07:22:02 +0200

    commit from home

--- MVC2017.Angular/src/app/app.component.ts
+++ MVC2017.Angular/src/app/app.component.ts
@@ -1,10 +1,15 @@
 ﻿import { Component } from '@angular/core';
 
 @Component({
+    
     selector: 'app-root',
     templateUrl: './app.component.html',
     //template:`mahmoud ahmed {{title}}`
-    styleUrls: ['./app.component.css']
+    //styleUrls: ['./app.component.css']
+    styles: [`h3
+{
+color:green;
+}`]
 })
 export class AppComponent {
     title = 'Mahmoud app';
--- MVC2017.Angular/src/app/servers/servers.component.ts
+++ MVC2017.Angular/src/app/servers/servers.component.ts
@@ -1,12 +1,14 @@
 ﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  //templateUrl: './servers.component.html',
-  template: 
- `<app-server></app-server>
-inline template
-<app-server></app-server>`,
+    //selector: '[app-servers]',
+    selector: '.app-servers',
+  //selector: 'app-servers',
+  templateUrl: './servers.component.html',
+//  template: 
+// `<app-server></app-server>
+//inline template
+//<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
