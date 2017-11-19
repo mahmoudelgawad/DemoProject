d4cdc13a949c227b15c37095db6089700b9798bc
mahmoud <memodnet@gmail.com>
2017-11-19 07:09:13 +0200

    from home

--- MVC2017.Angular/src/app/app.module.ts
+++ MVC2017.Angular/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent} from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
       AppComponent,
       ServerComponent,
-      ServersComponent
+      ServersComponent,
+      HeaderComponent,
+      RecipesComponent,
+      RecipeListComponent,
+      RecipeItemComponent,
+      ShopingListComponent,
+      ShopingEditComponent
   ],
   imports: [
       BrowserModule,
