204aa112aa8ba7b6bb676225dc26c77c125da0ef
mahmoud ahmed <mahmoud.elgawad@gmail.com>
2018-03-17 22:58:28 +0200

    add users table in test db change api users objects from home

--- DemoProject.Entity/DataModel/testEntityDataModel.Designer.cs
+++ DemoProject.Entity/DataModel/testEntityDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\MyLab\MVC2017\DemoProject.Entity\DataModel\testEntityDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\AA Drive\MVC2017\DemoProject.Entity\DataModel\testEntityDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
