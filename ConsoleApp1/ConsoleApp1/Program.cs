611771856c646f156c78615090aecdb518ca7dc9
Mahmoud Ahmed <mahmoud.elgawad@gmail.com>
2018-03-22 16:51:03 +0200

    from work

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,16 +11,30 @@ namespace ConsoleApp1
     class Program
     {
 
+        public static void rotate(int[] nums, int k) {
+            if (k > nums.Length)
+                k = k % nums.Length;
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < k; i++) {
+                result[i] = nums[nums.Length - k + i];
+            }
+            int j = 0;
+            for (int i = k; i < nums.Length; i++) {
+                result[i] = nums[j];
+                j++;
+            }
+            Array.Copy(result, nums, nums.Length);
+        }
         static void Main(string[] args)
         {
-            int month = 2;
-            int year = 2001;
-            DateTime date = new DateTime(year, month, 1);
-            Console.WriteLine(date.ToString("yy") + "      " + date.ToString("MM"));
-            Console.Read();
+            int[] solution = new int[] { 5, 6, 7, 1, 2, 3, 4 };
+                rotate(solution, 3);
+            Console.WriteLine("");
+           
         }
 
-      
+
 
 
 
