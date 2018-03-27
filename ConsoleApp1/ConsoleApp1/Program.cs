492183f87b40255bf1afcff2d783ff5e4e32bea7
mahmoud ahmed <mahmoud.elgawad@gmail.com>
2018-03-27 13:43:29 +0200

    commit from home

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,17 +10,33 @@ namespace ConsoleApp1
 
     class Program
     {
-
+        public static void RigthRotate(int[] A, int K)
+        {
+            if (A.Length == 0 || K <= 0)
+            {
+                return;
+            }
+            int len = A.Length - 1;
+            while (K > 0)
+            {
+                int lastValue = A[len];
+                for (int i = len; i > 0; i--)
+                {
+                    A[i] = A[i - 1];
+                }
+                A[0] = lastValue;
+                --K;
+            }
+        }
         static void Main(string[] args)
         {
-            int month = 2;
-            int year = 2001;
-            DateTime date = new DateTime(year, month, 1);
-            Console.WriteLine(date.ToString("yy") + "      " + date.ToString("MM"));
-            Console.Read();
+            int[] A = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            RigthRotate(A, 4);
+            Console.WriteLine(string.Join(",",A));
+            Console.WriteLine("End");
         }
 
-      
+
 
 
 
--- Demo.React/src/index.js
+++ Demo.React/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const API_KEY="AIzaSyDlw_E_mwaDv27xK9zVMMxo-5JjbwQbPcI";
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
